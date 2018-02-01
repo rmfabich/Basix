94f2b1dc5095122d16884db58e571d8ab15ce5ba
kramduckner <buckner.a.mark@gmail.com>
2018-02-01 16:12:08 -0700

    Create integerStuff.js

---
+++ integerStuff.js
@@ -0,0 +1,17 @@
+//make the code work below by writing a function called "bananas" that accepts two integers and returns the sum of them. 
+
+console.log(bananas(3, 4));
+//7
+console.log(bananas(666, 777));
+//1443
+console.log(bananas(1,0));
+//1
+
+
+
+//write a new function (call it whatever you want) that accepts three arguments - a string, an int, and another int. 
+//The string will be a person's name. The function should return a string that says "Hello {name}, the product of your 
+//numbers is {product of the two ints}." Console log the output to verify your function works. Keep in mind, the console log 
+//should NOT be inside your function. Feel free to put console logs in there to test your code, but the final product should
+//lack any console logs inside the function you create.
+
