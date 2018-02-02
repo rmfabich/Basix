2cf64c2b48062a79abd794d92dd618755e59f94c
kramduckner <buckner.a.mark@gmail.com>
2018-02-01 18:06:04 -0700

    Create logik.js

---
+++ logik.js
@@ -0,0 +1,13 @@
+//make the function work
+
+var isCat = function(animal){
+  if (){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isCat("dragon"));
+console.log(isCat("cat"));
+console.log(isCat("dog"));
