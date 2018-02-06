68ec80fb2524382684f279e4f476f99cdc46b812
kramduckner <buckner.a.mark@gmail.com>
2018-02-06 15:31:32 -0700

    Create HTML.js

---
+++ HTML.js
@@ -0,0 +1,16 @@
+Create an HTML page, served using http-server. 
+
+Using HTML, create a table that contains:
+  3 columns
+  3 rows
+  table headers for each column
+ populate it with whatever data you feel like
+  
+Using HTML, create a List (ordered or unordered)
+  that contains your most hated foods
+  
+Using HTML, 
+  make a title for the page appropritate for the content you created. 
+  
+  
+ Style up your webpage with css. Use classes, ids, whatever. Have fun with it. Make it look awful. It's very easy to do. 
